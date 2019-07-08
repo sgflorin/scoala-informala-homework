a64a3f0a606df0e2728527df5bd2a5aea696f2b5
sgflorin <sovaialagflorin@gmail.com>
2019-07-08 20:08:36 +0300

    Added homework idea project + change

--- src/ro/siit/tema1/TestClass.java
+++ src/ro/siit/tema1/TestClass.java
@@ -3,5 +3,6 @@ package ro.siit.tema1;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("GIT Test");
+        System.out.println("Test 2");
     }
 }
